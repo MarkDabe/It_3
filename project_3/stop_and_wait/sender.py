cc6221c9a2aa20d6e3f83f33e8234a59c23249db
markhabashy <markhabashy91@gmail.com>
2019-08-12 20:45:57 -0400

    beta version

--- project_3/stop_and_wait/sender.py
+++ project_3/stop_and_wait/sender.py
@@ -13,10 +13,9 @@ print('Sender ready and is listening for receiver to connect')
 
 while 1:
 
-    for number in range(1, 11):
-        give_up = False
+    clientsocket, address = serversocket.accept()
 
-        clientsocket, address = serversocket.accept()
+    for number in range(0, 10):
 
         clientsocket.send(bytes('hello:{}'.format(number), 'utf8'))
 
@@ -28,16 +27,9 @@ while 1:
 
         while buffer != "ack:{}".format(number):
             time.sleep(1)
-            clientsocket.close()
-            clientsocket, address = serversocket.accept()
-            clientsocket.send(bytes('hello:{}'.format(number), 'utf8'))
+            # clientsocket.close()
+            # clientsocket, address = serversocket.accept()
+            # clientsocket.send(bytes('hello:{}'.format(number), 'utf8'))
             buffer = clientsocket.recv(1024).decode('utf8')
-            # if time.time() - start < 10:
-            #     print("timeout")
-            #     give_up = True
-            #     break
 
-        clientsocket.close()
-
-        # if give_up:
-        #     break
+    clientsocket.close()
--- stop_and_wait/sender.py
+++ stop_and_wait/sender.py
@@ -13,10 +13,9 @@ print('Sender ready and is listening for receiver to connect')
 
 while 1:
 
-    for number in range(0, 10):
-        give_up = False
+    clientsocket, address = serversocket.accept()
 
-        clientsocket, address = serversocket.accept()
+    for number in range(0, 10):
 
         clientsocket.send(bytes('hello:{}'.format(number), 'utf8'))
 
@@ -28,9 +27,9 @@ while 1:
 
         while buffer != "ack:{}".format(number):
             time.sleep(1)
-            clientsocket.close()
-            clientsocket, address = serversocket.accept()
-            clientsocket.send(bytes('hello:{}'.format(number), 'utf8'))
+            # clientsocket.close()
+            # clientsocket, address = serversocket.accept()
+            # clientsocket.send(bytes('hello:{}'.format(number), 'utf8'))
             buffer = clientsocket.recv(1024).decode('utf8')
 
-        clientsocket.close()
+    clientsocket.close()
